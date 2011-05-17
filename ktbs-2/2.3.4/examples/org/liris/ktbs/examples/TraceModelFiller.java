cc082d6ee24d58140d60f62626bdf5b8032ef503
dcram <dcram@2a3803fa-5b2e-0410-8379-b254d7af8764>
2011-05-17 16:48:22 +0000

    Remove the dist dir that was added accidentaly
    git-svn-id: https://svn.liris.cnrs.fr/dcram/dev/ktbs@894 2a3803fa-5b2e-0410-8379-b254d7af8764

--- ktbs-2/2.3.4/examples/org/liris/ktbs/examples/TraceModelFiller.java
+++
@@ -1,8 +0,0 @@
-package org.liris.ktbs.examples;
-
-import org.liris.ktbs.domain.PojoFactory;
-import org.liris.ktbs.domain.interfaces.ITraceModel;
-
-public abstract class TraceModelFiller {
-	public abstract void fill(ITraceModel model, PojoFactory factory);
-}
--- ktbs-2/2.3.4/examples/run-examples.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-CLASSPATH=.:../*:../lib/*
-
-echo "Classpath: $CLASSPATH"
-
-java -cp $CLASSPATH org.liris.ktbs.examples.$1 $2
-
-

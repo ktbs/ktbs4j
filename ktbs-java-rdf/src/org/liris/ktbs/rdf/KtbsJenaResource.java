3154d7224dac43b8bf6f5f3d1fb63a77cbff5078
dcram <dcram@2a3803fa-5b2e-0410-8379-b254d7af8764>
2011-05-24 09:08:07 +0000

    git-svn-id: https://svn.liris.cnrs.fr/dcram/dev/ktbs@900 2a3803fa-5b2e-0410-8379-b254d7af8764

--- ktbs-java-rdf/src/org/liris/ktbs/rdf/KtbsJenaResource.java
+++
@@ -1,7 +0,0 @@
-package org.liris.ktbs.rdf;
-
-import com.hp.hpl.jena.rdf.model.Model;
-
-public interface KtbsJenaResource {
-	public Model getCopyOfModel();
-}
--- visu2-import/src/org/liris/ktbs/visu2/Visu2TraceModel.java
+++
@@ -1,7 +0,0 @@
-package org.liris.ktbs.visu2;
-
-public class Visu2TraceModel {
-	public static final String OT_RETRO_ = "";
-	
-
-}
